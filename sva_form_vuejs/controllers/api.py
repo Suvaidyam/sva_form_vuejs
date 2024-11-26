a58d78a313c09deb9066a2b6e00c8ecca10ee101
rsuvaidyam <rkrahul00011@gmail.com>
2024-11-26 07:43:49 +0000

    add

--- sva_form_vuejs/controllers/api.py
+++ sva_form_vuejs/controllers/api.py
@@ -6,9 +6,12 @@ def get_meta(doctype):
 
 @frappe.whitelist(allow_guest=True)
 def get_option(filters):
-    return frappe.get_all('Field Options',filters=filters, fields=['name', 'label', 'code as level' ,"*"] , order_by='code')
+    return frappe.get_all('Field Options',filters=filters, fields=['name', 'label', 'code as level' ,'depends_on'] , order_by='code')
 
 @frappe.whitelist(allow_guest=True)
 def get_option_with_dt(dt,filters=[]):
     meta = frappe.get_meta(dt).as_dict()
-    return frappe.get_all(dt,filters=filters, fields=['name', f"{meta['title_field']} as label"])
+    if meta['title_field']:
+        return frappe.get_all(dt,filters=filters, fields=['name', f"{meta['title_field']} as label"])
+    else:
+        return frappe.get_all(dt,filters=filters, fields=['name', 'name as label'])
